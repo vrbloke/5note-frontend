726c25829b31815fd27c3b82bfeab0612470b00f
vrbloke <stanislawwis@gmail.com>
2022-05-16 16:06:34 +0200

    Project in shambles, make available to team

--- src/routes/helpers.js
+++ src/routes/helpers.js
@@ -1,3 +1,6 @@
+export const api = 'http://localhost:8080'
+export const saltN = 5
+
 export function isLoggedIn() {
     return false
 }
--- src/routes/login/login.js
+++ src/routes/login/login.js
@@ -1,24 +1,57 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
+import bcrypt from "bcryptjs";
+import {api, saltN} from "../helpers";
 
 export function Login() {
     const [actionType, setActionType] = useState('');
     const [loginValue, setLoginValue] = useState();
     const [passwordValue, setPasswordValue] = useState();
+    const [message, setMessage] = useState('');
 
     useEffect(function handleSubmit() {
+        console.log(`Action: ${actionType}\nLogin: ${loginValue}\nPassword: ${passwordValue}`)
         switch(actionType) {
             case '':
                 break
             case 'login':
-            case 'register':
-                console.log(actionType)
-                console.log(loginValue)
-                console.log(passwordValue)
-                axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
-                    .then(re => {
-                        console.log(re.data)
+                setActionType('')
+                axios.get(api+`/users/search/findByUsername?username=${loginValue}`)
+                    .then(res => {
+                        return axios.post(api+`/login?username=${loginValue}&password=${passwordValue}`)
                     })
+                    .then(res => {
+                        if(!(res.status === 200)) {
+                            setMessage('Błędne hasło')
+                        }
+                        else {
+                            setMessage('Logowanie udane!')
+                        }
+                    })
+                    .catch(err => {
+                        switch(err.response.status) {
+                            case 404:
+                                setMessage('Nie istnieje użytkownik o takiej nazwie')
+                                break
+                            default:
+                                setMessage('Nie można przeprowadzić autentykacji')
+                        }
+                    })
+                break
+            case 'register':
+                setActionType('')
+                bcrypt.hash(passwordValue, saltN, (err,h) => {
+                    if(err) {setMessage('Błąd w rejestracji')}
+                    console.log(h);
+                    axios.post(api+'/users', {username: loginValue, hash: h})
+                        .then(res => {
+                            console.log(res)
+                        })
+                        .catch(err => {
+                            setMessage('Błąd w rejestracji')
+                        })
+                })
+                break
         }
     }, [actionType])
 
@@ -46,7 +79,7 @@ export function Login() {
                     </label>
                     <input type="button" value="Zaloguj" id="submit-login" className="button" onClick={submitLogin}/>
                     <input type="button" value="Rejestracja" id="submit-register" className="button" onClick={submitRegister}/>
-                    <p>Current state: {actionType}, {loginValue}, {passwordValue}</p>
+                    <p>Message: {message}</p>
                 </form>
             </div>
         </div>
